d74966f42644eeb93d3c3a580d88a8775170a43b
LaGz4643 <89419693+LoganHauser@users.noreply.github.com>
2022-10-24 12:40:17 -0600

    Create CustomObject.java

---
+++ CustomObject.java
@@ -0,0 +1,18 @@
+public class CustomObject {
+
+    //Create 3 attributes
+    String attribute1;
+    int attribute2;
+    int attribute3;
+    
+    //Constructor
+    public CustomObject(String attribute1, int attribute2, int attribute3) {
+        this.attribute1 = attribute1;
+        this.attribute2 = attribute2;
+        this.attribute3 = attribute3;
+    }
+
+    public void behavior(int number) {
+        attribute2 = number;
+    }
+}
